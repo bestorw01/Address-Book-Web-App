5e15817ace57dfa3d3e507e5d8a66f375517c2df
Mentor <bestorw@hotmail.com>
2018-04-10 12:45:45 -0700

    My First Commit

---
+++ Andela Add Book/script/add.js
@@ -0,0 +1,11 @@
+if (localStorage.contacts == null) {
+  localStorage.contacts = JSON.stringify([]);
+}
+if (localStorage.idOfContactBeingEdited == null) {
+  localStorage.idOfContactBeingEdited = JSON.stringify(0);
+}
+
+window.onload = function(){
+	//Button
+
+}
